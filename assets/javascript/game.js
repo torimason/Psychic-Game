e2465f26f86b071b1fc652c700cd2fe877e704d4
Tori Mason <tori.mason@sbcglobal.net>
2018-06-23 11:02:08 -0500

    final files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,7 +10,7 @@ var computerChoices = ["a" , "b" , "c" , "d" , "e" , "f" , "g" , "h" , "i" , "j"
         var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
         
-        
+        if ((userGuess === "a") || (userGuess === "b") || (userGuess === "c") || (userGuess === "d") || (userGuess === "e") || (userGuess === "f") || (userGuess === "g") || (userGuess === "h") || (userGuess === "i") || (userGuess === "j") || (userGuess === "k") || (userGuess === "l") || (userGuess === "m") || (userGuess === "n") || (userGuess === "o") || (userGuess === "p") || (userGuess === "q") || (userGuess === "r") || (userGuess === "s") || (userGuess === "t") || (userGuess === "u") || (userGuess === "v") || (userGuess === "w") || (userGuess === "x") || (userGuess === "y") ||(userGuess === "z")){ 
             if (userGuess === computerGuess) {
             wins++
             }
@@ -26,9 +26,12 @@ var computerChoices = ["a" , "b" , "c" , "d" , "e" , "f" , "g" , "h" , "i" , "j"
             "<p>Your Losses: " + losses + "</p>";
 
             document.querySelector("#game").innerHTML = html;
+        }
+
+        else {alert("Please select letter keys only.")};
             
 
-        
+        //need to make it to where users can only select letter keys.
 
        
     
